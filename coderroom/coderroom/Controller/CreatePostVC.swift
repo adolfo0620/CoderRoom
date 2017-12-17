0a52e35dfe0eb5359d29761ac4cba2aeb6682eff
Adolfo Reyes <adolfo0620@gmail.com>
2017-12-17 08:35:36 -0500

    users can now sent post

---
+++ coderroom/coderroom/Controller/CreatePostVC.swift
@@ -0,0 +1,54 @@
+//
+//  CreatePostVC.swift
+//  coderroom
+//
+//  Created by adolfo reyes on 12/17/17.
+//  Copyright © 2017 adolfo reyes. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class CreatePostVC: UIViewController {
+
+    
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var sendBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sendBtnWasPressed(_ sender: Any) {
+        if textView.text != nil && textView.text != "say something here"{
+            sendBtn.isEnabled = false
+            DataService.instance.uploadPost(withMessage: textView.text, forUID: (Auth.auth().currentUser?.uid)!, withGroupKey: nil, sendComplete: { (isComplete) in
+                if isComplete {
+                    self.sendBtn.isEnabled = true
+                    self.dismiss(animated: true, completion: nil)
+                }else{
+                    self.sendBtn.isEnabled = true
+                    print("An Error occurred during sending")
+                }
+            })
+        }
+    }
+    
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension CreatePostVC: UITextViewDelegate{
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+    }
+}
